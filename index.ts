3d6d35ac5d13a0dbe05c5bceeb60ffd8cb52c40c
Samer Kayali <samerkkr@gmail.com>
2021-09-30 21:16:51 -0300

    feat: added class method easier to manipulate query params

--- index.ts
+++ index.ts
@@ -244,7 +244,40 @@ export default function urlSearchParamsFactory<
   };
 }
 
-export class urlSearchParamsBuilder<
+/**
+ *@description           [A helper to deal with url search params]
+
+
+  @example
+  // Manipulate params
+    const newUrlSearchParams = urlSearchParamsHelper.create("?key1=value1&key2=value2&key3=value3&key4=value4")
+    .addParam({ key:"addedkey", value:"addedValue"})
+    .removeParam('key1')
+    .addOrReplaceParam({key:"key3", value:"replacedValue"})
+
+    const params = newUrlSearchParams.getAllParams()
+    console.log(params) // { addedkey:"addedValue", key2:"value2",key3:"replacedValue", key4:"value4" }
+
+    const urlSearchParams = newUrlSearchParams.getUrlSearchParams()
+    console.log(urlSearchParams) // "addedkey=addedValue&key2=value2&key3=replacedValue&key4=value4"
+
+    --- Typescript ---
+
+    const newUrlSearchParams = urlSearchParamsHelper
+    .create<{ key1?: string, key2:string, key3:string, key4:string, addedKey?:string }>("?key1=value1&key2=value2&key3=value3&key4=value4")
+    .addParam({ key:"addedkey", value:"addedValue"})
+    .removeParam('key1')
+    .addOrReplaceParam({key:"key3", value:"replacedValue"})
+
+    const params = newUrlSearchParams.getAllParams()
+    console.log(params) // { addedkey:"addedValue", key2:"value2",key3:"replacedValue", key4:"value4" }
+
+    const urlSearchParams = newUrlSearchParams.getUrlSearchParams()
+    console.log(urlSearchParams) // "addedkey=addedValue&key2=value2&key3=replacedValue&key4=value4"
+
+ */
+
+export class urlSearchParamsHelper<
   T extends Record<string, string> = Record<string, string>
 > {
   urlSearchParams: URLSearchParams;
@@ -255,21 +288,33 @@ export class urlSearchParamsBuilder<
 
   static create<K extends Record<string, string> = Record<string, string>>(
     queryUrl = ""
-  ): urlSearchParamsBuilder<K> {
-    return new urlSearchParamsBuilder<K>(queryUrl);
+  ): urlSearchParamsHelper<K> {
+    return new urlSearchParamsHelper<K>(queryUrl);
   }
 
   /**
    * @description                       [add single param to url search params]
-   *
    * @param   {string}        key       [the key of the param to add]
    * @param   {string}        value     [the value the param to add]
    *
    * @return  {string}                  [Url search params string modified]
    *
    * @example
-   *  const { addParam } = urlSearchParamsFactory('?key1=value1')
-   *  const urlSearchParamsWithAddedParam = addParam({ key:"key2", value:"value2"})
+   *  const urlSearchParamsWithAddedParam = urlSearchParamsHelper
+   *  .create('?key1=value1')
+   *  .addParam({ key:"key2", value:"value2"})
+   *  .getUrlSearchParams()
+   *
+   *
+   *  console.log(urlSearchParamsWithAddedParam) => "key1=value1&key2=value2"
+   *
+   *  --- Typescript ---
+   *
+   *   const urlSearchParamsWithAddedParam = urlSearchParamsHelper
+   *  .create<{ key1:string ,key2?: string }>('?key1=value1')
+   *  .addParam({ key:"key2", value:"value2"})
+   *  .getUrlSearchParams()
+   *
    *
    *  console.log(urlSearchParamsWithAddedParam) => "key1=value1&key2=value2"
    *
@@ -280,7 +325,7 @@ export class urlSearchParamsBuilder<
   }: {
     key: string;
     value: string;
-  }): urlSearchParamsBuilder<T> {
+  }): urlSearchParamsHelper<T> {
     this.urlSearchParams.append(key, value);
     return this;
   }
@@ -293,19 +338,31 @@ export class urlSearchParamsBuilder<
    * @return  {string}                     [Url search params string modified]
    *
    * @example
-   *  const { addParamList } = urlSearchParamsFactory('?key1=value1')
-   *  const urlSearchParamsWithAddedParamList = addParamList([{ key:"key2", value:"value2"},{ key:"key3", value:"value3"}])
+   *  const urlSearchParamsWithAddedParamList = urlSearchParamsHelper
+   * .create('?key1=value1')
+   * .addParamList([{ key:"key2", value:"value2"},{ key:"key3", value:"value3"}])
+   * ..getUrlSearchParams()
+   *
+   *  console.log(urlSearchParamsWithAddedParamList) => "key1=value1&key2=value2&key3=value3"
+   *
+   * --- Typescript ---
+   *
+   *  const urlSearchParamsWithAddedParamList = urlSearchParamsHelper
+   * .create<{ key1:string, key2?: string, key3:string  }>('?key1=value1')
+   * .addParamList([{ key:"key2", value:"value2"},{ key:"key3", value:"value3"}])
+   * .getUrlSearchParams()
    *
    *  console.log(urlSearchParamsWithAddedParamList) => "key1=value1&key2=value2&key3=value3"
    */
   addParamList(
     paramsList: { key: string; value: string }[]
-  ): urlSearchParamsBuilder<T> {
+  ): urlSearchParamsHelper<T> {
     paramsList.forEach(({ key, value }) => {
       this.urlSearchParams.append(key as string, value);
     });
     return this;
   }
+
   /**
    * @description           [remove specific param]
    *
@@ -314,16 +371,29 @@ export class urlSearchParamsBuilder<
    * @return  {string}      [Url search params string modified]
    *
    * @example
-   *  const { removeParam } = urlSearchParamsFactory('?key1=value1&key2=value2&key3=value3&key4=value4')
-   *  const urlSearchParamsWithRemovedParam = removeParam("key2")
+   *  const urlSearchParamsWithRemovedParam = urlSearchParamsHelper
+   * .create('?key1=value1&key2=value2&key3=value3&key4=value4')
+   * .removeParam("key2")
+   * .getUrlSearchParams()
+   *
+   *  console.log(urlSearchParamsWithRemovedParam) => "key1=value1&key3=value3&key4=value4"
+   *
+   *  --- Typescript ---
+   *
+   * const urlSearchParamsWithRemovedParam = urlSearchParamsHelper
+   * .create<{ key1:string key2?string, key3:string, key4:string }>('?key1=value1&key2=value2&key3=value3&key4=value4')
+   * .removeParam("key2")
+   * .getUrlSearchParams()
    *
    *  console.log(urlSearchParamsWithRemovedParam) => "key1=value1&key3=value3&key4=value4"
    *
    */
-  removeParam(key: keyof T): urlSearchParamsBuilder<T> {
+
+  removeParam(key: keyof T): urlSearchParamsHelper<T> {
     this.urlSearchParams.delete(key as string);
     return this;
   }
+
   /**
    * @description                               [remove params list specified by an array of strings representing the key]
    *
@@ -332,12 +402,26 @@ export class urlSearchParamsBuilder<
    * @return  {string}                        [Url search params string modified]
    *
    * @example
-   *  const { removeParamList } = urlSearchParamsFactory('?key1=value1&key2=value2&key3=value3&key4=value4')
-   *  const urlSearchParamsWithRemovedParamList = removeParamList(["key2", "key3"])
+   *  const urlSearchParamsWithRemovedParamList = urlSearchParamsHelper
+   *  .create('?key1=value1&key2=value2&key3=value3&key4=value4')
+   *  .removeParamList(["key2", "key3"])
+   *  .getUrlSearchParams()
    *
    *  console.log(urlSearchParamsWithRemovedParamList) => "key1=value1&key4=value4"
+   *
+   *  --- Typescript ---
+   *
+   *  const urlSearchParamsWithRemovedParamList = urlSearchParamsHelper
+   *  .create<{ key1:string key2?string, key3?:string, key4?:string }>('?key1=value1&key2=value2&key3=value3&key4=value4')
+   *  .removeParamList(["key2", "key3"])
+   *  .getUrlSearchParams()
+   *
+   *  console.log(urlSearchParamsWithRemovedParamList) => "key1=value1&key4=value4"
+   *
+   *
    */
-  removeParamList(paramsList: (keyof T)[]): urlSearchParamsBuilder<T> {
+
+  removeParamList(paramsList: (keyof T)[]): urlSearchParamsHelper<T> {
     paramsList.forEach((key) => {
       this.urlSearchParams.delete(key as string);
     });
@@ -352,21 +436,34 @@ export class urlSearchParamsBuilder<
    * @return  {string}                             [Url search params string modified]
    *
    * @example
-   *  const { addOrReplaceParam } = urlSearchParamsFactory('?key1=value1&key2=value2')
-   *  const urlSearchParamsWithAddedOrReplacedParam = addOrReplaceParam({key:"key2", value:"replacedValue2"})
+   *  const urlSearchParamsWithAddedOrReplacedParam = urlSearchParamsHelper
+   * .create('?key1=value1&key2=value2')
+   * .addOrReplaceParam({key:"key2", value:"replacedValue2"})
+   * .getUrlSearchParams()
+   *
+   *  console.log(urlSearchParamsWithAddedOrReplacedParam) => "key1=value1&key2=replacedValue2"
+   *
+   * --- Typescript ---
+   *
+   * const urlSearchParamsWithAddedOrReplacedParam = urlSearchParamsHelper
+   * .create<{ key1:string, key2:string }>('?key1=value1&key2=value2')
+   * .addOrReplaceParam({key:"key2", value:"replacedValue2"})
+   * .getUrlSearchParams()
    *
    *  console.log(urlSearchParamsWithAddedOrReplacedParam) => "key1=value1&key2=replacedValue2"
    */
+
   addOrReplaceParam({
     key,
     value,
   }: {
     key: keyof T;
     value: string;
-  }): urlSearchParamsBuilder<T> {
+  }): urlSearchParamsHelper<T> {
     this.urlSearchParams.set(key as string, value);
     return this;
   }
+
   /**
    * @description                                                [if param exists it replaces the param, if it does not exists it adds param]
    *
@@ -375,14 +472,29 @@ export class urlSearchParamsBuilder<
    * @return  {string}                                           [Url search params string modified]
    *
    * @example
-   *  const { addOrReplaceParamList } = urlSearchParamsFactory('?key1=value1&key2=value2')
-   *  const urlSearchParamWithAddedAndReplacedParam = addOrReplaceParamList([{key:"key2", value:"replacedValue2"},{key:"key3", value:"addedValue3"}])
+   *  const urlSearchParamWithAddedAndReplacedParam = urlSearchParamsHelper
+   *  .create('?key1=value1&key2=value2')
+   *  .addOrReplaceParamList([{key:"key2", value:"replacedValue2"},{key:"key3", value:"addedValue3"}])
+   *  .getUrlSearchParams()
+   *
    *
    *  console.log(urlSearchParamWithAddedAndReplacedParam) => "key1=value1&key2=replacedValue2&key3=addedValue3"
+   *
+   * --- Typescript ---
+   *
+   *  const urlSearchParamWithAddedAndReplacedParam = urlSearchParamsHelper
+   *  .create<{ key1:string, key2:string, key3:string }>('?key1=value1&key2=value2')
+   *  .addOrReplaceParamList([{key:"key2", value:"replacedValue2"},{key:"key3", value:"addedValue3"}])
+   *  .getUrlSearchParams()
+   *
+   *
+   *  console.log(urlSearchParamWithAddedAndReplacedParam) => "key1=value1&key2=replacedValue2&key3=addedValue3"
+   *
    */
+
   addOrReplaceParamList(
     paramsList: { key: keyof T; value: string }[]
-  ): urlSearchParamsBuilder<T> {
+  ): urlSearchParamsHelper<T> {
     paramsList.forEach(({ key, value }) => {
       this.urlSearchParams.set(key as string, value);
     });
@@ -396,15 +508,26 @@ export class urlSearchParamsBuilder<
    * @return  {string}       [Url search params string modified]
    *
    * @example
-   *  const { getParam } = urlSearchParamsFactory('?key1=value1&key2=value2')
-   *  const param = getParam("key2")
+   *  const param = urlSearchParamsHelper
+   *  .create('?key1=value1&key2=value2')
+   *  .getParam("key2")
    *
-   *  console.log(param) => "value2"
+   *   console.log(param) => "value2"
+   *
+   * --- Typescript ---
+   *
+   * const param = urlSearchParamsHelper
+   *  .create<{ key1:string, key2:string }>('?key1=value1&key2=value2')
+   *  .getParam("key2")
+   *
+   *   console.log(param) => "value2"
    *
    */
+
   getParam(key: keyof T): string | null {
     return this.urlSearchParams.get(key as string);
   }
+
   /**
    * @description                              [get param list on url search params]
    *
@@ -412,17 +535,28 @@ export class urlSearchParamsBuilder<
    *
    * @return  {string}                          [object containing param values]
    * @example
-   *  const { getParamList } = urlSearchParamsFactory('?key1=value1&key2=value2&key3=value3')
-   *  const params = getParamList(["key2","key3"])
+   *  const params = urlSearchParamsHelper
+   * .create('?key1=value1&key2=value2&key3=value3')
+   * .getParamList(["key2","key3"])
+   *
+   *  console.log(params) => { key2:"value2", key3:"value3"};
+   *
+   * --- Typescript ---
+   *
+   *  const params = urlSearchParamsHelper
+   * .create<{ key1:string, key2:string, key3:string }>('?key1=value1&key2=value2&key3=value3')
+   * .getParamList(["key2","key3"])
    *
    *  console.log(params) => { key2:"value2", key3:"value3"};
    *
    */
+
   getParamList(keys: Array<keyof T>): Record<keyof T, string | null> {
     return keys.reduce((acc, key) => {
       return { ...acc, [key]: this.urlSearchParams.get(key as string) };
     }, {}) as Record<keyof T, string | null>;
   }
+
   /**
    * @description                                                   [get all params on url search params]
    *
@@ -430,11 +564,22 @@ export class urlSearchParamsBuilder<
    * @return  {Record<string, string>}                              [object containing all param values]
    *
    *  @example
-   *  const { getAllParams } = urlSearchParamsFactory('?key1=value1&key2=value2&key3=value3')
-   *  const params = getAllParams()
+   *  const params = urlSearchParamsHelper
+   *  .create('?key1=value1&key2=value2&key3=value3')
+   *  .getAllParams()
    *
    *  console.log(params) => { key1:"value1", key2:"value2", key3:"value3" };
+   *
+   *  --- Typescript ---
+   *
+   * const params = urlSearchParamsHelper
+   *  .create<{ key1:string, key2:string, key3:string }>('?key1=value1&key2=value2&key3=value3')
+   *  .getAllParams()
+   *
+   *  console.log(params) => { key1:"value1", key2:"value2", key3:"value3" };
+   *
    */
+
   getAllParams(): Record<keyof T, string> | Record<string, never> {
     const entries = this.urlSearchParams.entries();
     let entriesValues = entries.next();
@@ -455,34 +600,24 @@ export class urlSearchParamsBuilder<
    * @return  {string}  [url search params]
    *
    * @example
-   *  const { getQueryParamUrl } = urlSearchParamsFactory('?key1=value1&key2=value2&key3=value3')
-   *  const queryParamUrl = getQueryParamUrl()
+   *  const queryParamUrl = urlSearchParamsHelper
+   *  .create('?key1=value1&key2=value2&key3=value3')
+   *  .getQueryParamUrl()
+   *
    *
    *  console.log(queryParamUrl) => "key1=value1&key2=value2&key3=value3";
+   *
+   *  --- Typescript ---
+   *
+   *  const queryParamUrl = urlSearchParamsHelper
+   *  .create<{ key1:string, key2:string, key3:string }>('?key1=value1&key2=value2&key3=value3')
+   *  .getQueryParamUrl()
+   *
+   *
+   *  console.log(queryParamUrl) => "key1=value1&key2=value2&key3=value3";
+   *
    */
   getUrlSearchParams(): string {
     return this.urlSearchParams.toString();
   }
-  //
-  //
-  // };
 }
-
-const { addParam, compose, getParamList } =
-  urlSearchParamsFactory<{ samer: string }>("?samin=dasGata");
-
-const newUrl = compose(
-  () => addParam({ key: "samer", value: "sameroso" }),
-  () => addParam({ key: "para", value: "tudo" })
-);
-
-const ty = getParamList(["samer"]);
-
-console.log(newUrl);
-
-const x = urlSearchParamsBuilder
-  .create("?samin=dasGata")
-  .addParamList([{ key: "doido", value: "foi" }])
-  .getParam("doido");
-
-console.log(x);
